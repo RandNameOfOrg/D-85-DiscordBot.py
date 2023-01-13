e5233a5b3e65f2eb17010cb87ea22ec9c02ad33a
MGS-Daniil <105662394+MGS-Daniil@users.noreply.github.com>
2023-01-13 19:03:01 +0300

    delete unused imports

--- SimpleBot.py
+++ SimpleBot.py
@@ -1,10 +1,5 @@
 from __future__ import print_function
 import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 #import cogs.file.reload as rl
 from discord.ext import commands, tasks
