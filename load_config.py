01853131379d98457bd770705a1faa77f8601518
Daniil <105662394+MGS-Daniil@users.noreply.github.com>
2023-10-02 21:19:30 +0300

    Update
    adding config auto-update system (beta)

---
+++ load_config.py
@@ -0,0 +1,6 @@
+import updating_files as uf
+
+
+def load():
+    uf.update()
+    uf.get()
---
+++ updating_files/__init__.py
@@ -0,0 +1,9 @@
+from update__check import check_for_updates
+
+
+def update():
+    return None
+
+
+def get():
+    return None
