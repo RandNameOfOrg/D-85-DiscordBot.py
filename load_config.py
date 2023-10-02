918e3914fa6704e6ded07641baea5ecdfa052a98
Daniil <105662394+MGS-Daniil@users.noreply.github.com>
2023-10-02 21:58:11 +0300

    Update

--- load_config.py
+++ load_config.py
@@ -1,6 +1,11 @@
+from configparser import ConfigParser
 import updating_files as uf
+from core.data import PATH_TO_CONFIG
 
+data = uf.AppData()
+config = ConfigParser()
+config.read(PATH_TO_CONFIG)
 
-def load():
-    uf.update()
-    uf.get()
+print(data())
+
+config.set("Settings", "VERSION", data().get("version"))
--- updating_files/__init__.py
+++ updating_files/__init__.py
@@ -1,9 +1,23 @@
+import json
+import os
+from pathlib import Path
+
 from update__check import check_for_updates
 
 
-def update():
-    return None
+class AppData:
+    def __init__(self):
+        self.data = {}
+        self.url = "https://raw.githubusercontent.com/MGS-Daniil/D-85-DiscordBot.py/main"
 
+    def __call__(self, *args, **kwargs):
+        return self.data
 
-def get():
-    return None
+    def get(self) -> dict:
+        self.update()
+        with open(Path('.json').absolute(), "r") as f:
+            data = json.load(f)
+        return data
+
+    def update(self) -> None:
+        check_for_updates(Path(".json").absolute(), f"{self.url}/updating_files/.json")
